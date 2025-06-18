50cc09beaaafd3620243d4f6f878bbad66748b31
maksimbuckov9 <maksimbuckov9@gmail.com>
2025-06-18 16:45:23 +0300

    Добавлены базовые формы в forms.py для валидации данных

--- payments/forms.py
+++ payments/forms.py
@@ -1,17 +1,7 @@
 from django import forms
-from .models import Payment, Course, Student
-
-class PaymentForm(forms.ModelForm):
-    class Meta:
-        model = Payment
-        fields = ['course', 'amount', 'status']
-
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['passport', 'phone']
+from .models import Course
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['name', 'description', 'teacher', 'price', 'start_date', 'end_date']
+        fields = '__all__'
