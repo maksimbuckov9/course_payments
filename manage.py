e2503e87090e3e4e09bfaa6a9eb2cfeebfca6b6c
maksimbuckov9 <maksimbuckov9@gmail.com>
2025-06-18 16:41:56 +0300

    Добавлены модели Course, Student, Payment, Contract

--- manage.py
+++ manage.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course_payments.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Не удалось импортировать Django. Убедитесь, что он установлен и доступен в PYTHONPATH."
-        ) from exc
+        raise ImportError("Couldn't import Django.") from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
@@ -11,32 +10,3 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    passport = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.user.get_full_name()
-
-class Payment(models.Model):
-    STATUS_CHOICES = [('paid', 'Оплачено'), ('pending', 'Ожидается')]
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
-
-    def __str__(self):
-        return f"{self.student} - {self.course} - {self.status}"
-
-class Contract(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    contract_number = models.CharField(max_length=50)
-    issue_date = models.DateField(auto_now_add=True)
-    file_path = models.FileField(upload_to='contracts/')
-
-    def __str__(self):
-        return self.contract_number
