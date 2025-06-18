60c9d166a591627507f7aefd2f6e44fb3d7a240b
maksimbuckov9 <maksimbuckov9@gmail.com>
2025-06-18 16:43:23 +0300

    Исправлены ошибки загрузки шаблонов путем корректировки путей

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('students/', views.students_list, name='students_list'),
-    # Добавим позже остальные пути
+    path('courses/', views.course_list, name='course_list'),
+    path('courses/add/', views.course_create, name='course_create'),
+    path('courses/<int:pk>/edit/', views.course_edit, name='course_edit'),
+    path('courses/<int:pk>/delete/', views.course_delete, name='course_delete'),
 ]
