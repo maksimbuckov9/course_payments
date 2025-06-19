435a04cdeefbda11075bf321174b619bbbe10de1
maksimbuckov9@gmail.com <maksimbuckov9@gmail.com>
2025-06-19 19:14:25 +0300

    Изменение модели данных, добавление студентов

--- payments/models.py
+++ payments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
@@ -7,6 +8,10 @@ class Course(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateField()
     end_date = models.DateField()
-
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    passport = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
