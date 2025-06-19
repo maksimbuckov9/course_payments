f926609986bd255c8e25250b941fd4f122b7cfaf
maksimbuckov9@gmail.com <maksimbuckov9@gmail.com>
2025-06-19 19:12:26 +0300

    Изменение маршрута

--- payments/urls.py
+++ payments/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('courses/add/', views.course_create, name='course_create'),
     path('courses/<int:pk>/edit/', views.course_edit, name='course_edit'),
     path('courses/<int:pk>/delete/', views.course_delete, name='course_delete'),
+    path('students/', views.student_list, name='student_list'),
+    path('students/add/', views.student_create, name='student_create'),
+    path('students/<int:pk>/edit/', views.student_edit, name='student_edit'),
+    path('students/<int:pk>/delete/', views.student_delete, name='student_delete'),
 ]
