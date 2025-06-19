1cc2803af14b9c4d8523f6501382935aa67e517e
maksimbuckov9@gmail.com <maksimbuckov9@gmail.com>
2025-06-19 19:14:43 +0300

    Изменение формы, добавление студентов

--- payments/forms.py
+++ payments/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Course
+from .models import Student
+from django.contrib.auth.models import User
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
+        fields = '__all__'
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
         fields = '__all__'
